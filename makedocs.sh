4487dec1ab001515c08180974fc587e0e4a76470
Mike Dilger <mike@mikedilger.com>
2025-08-06 09:32:32 +1200

    Document re-exported dependencies

--- makedocs.sh
+++ makedocs.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+rm -rf ./target/doc/
 RUSTDOCFLAGS="--cfg docsrs" cargo +nightly doc --all-features --no-deps
+RUSTDOCFLAGS="--cfg docsrs" cargo +nightly doc --no-deps -p secp256k1
+RUSTDOCFLAGS="--cfg docsrs" cargo +nightly doc --no-deps -p ed25519-dalek
+RUSTDOCFLAGS="--cfg docsrs" cargo +nightly doc --no-deps -p mainline
+RUSTDOCFLAGS="--cfg docsrs" cargo +nightly doc --no-deps -p rand
+
 echo "<meta http-equiv=\"refresh\" content=\"0; url=mosaic_core/index.html\">" > target/doc/index.html
 rm -rf ./docs
 mv target/doc ./docs
